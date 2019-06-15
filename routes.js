92b4941a110c334f7a3381bc41da5a7c35b35452
Your Name <you@example.com>
2019-06-15 13:07:11 +0200

    feat: add home path

--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
 
 function addRoute(server){
     console.log("Adding routes")
+
+    server.get("/", (req, res, next)=>{
+        res.send("gg")
+    })
+
+
 }
 
 module.exports = addRoute
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const initServer = require("./initServer")
-console.log("gg")
+const PORT = 3000
 
 const server = initServer()
+
+server.listen(PORT, ()=>console.log(`Listening on http://localhost:${PORT}`))

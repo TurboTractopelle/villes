0bcdb2158e2c7fe2827089ff237663a1bc730f8f
neroark <ns_ark@hotmail.com>
2019-06-18 21:24:10 +0200

    feat: add first letter search

--- models/Villes.js
+++ models/Villes.js
@@ -1,10 +1,22 @@
-const mongoose = require("mongoose")
-const db = require("../db/connection")
+const mongoose = require("mongoose");
+const db = require("../db/connection");
 
 const villesSchema = new mongoose.Schema({
-    name: String
-  })
+  name: String
+});
 
-const Villes = db.model('Villes', villesSchema)
+villesSchema.statics.search = search;
 
-module.exports = Villes
+async function search(letter = "") {
+  let data;
+  if (letter) {
+    data = await this.find({ name: { $regex: `^${letter}.+`, $options: "i" } });
+    return data;
+  }
+  data = await this.find({}, { name: 1 });
+  return data;
+}
+
+const Villes = db.model("Villes", villesSchema);
+
+module.exports = Villes;
--- routes.js
+++ routes.js
@@ -3,52 +3,53 @@ const getStuff = require("./mongoo");
 const Villes = require("./models/Villes");
 
 function addRoute(server) {
-	console.log("Adding routes");
+  console.log("Adding routes");
 
-	server.get("/villes", getVilles);
-	server.post("/addVilles", addVilles);
-	server.post("/addVille/:name", addVille);
-	server.get({ name: "test", path: "/test" }, testFn);
-	//server.post({ name: "test", path: "/test/:id" }, testPost);
-	server.post("/test/:id", testPost);
+  server.get("/villes", getVilles);
+  server.post("/addVilles", addVilles);
+  server.post("/addVille/:name", addVille);
+  server.get({ name: "test", path: "/test" }, testFn);
+  server.post("/test/:id", testPost);
 }
 
 async function getVilles(req, res, next) {
-	const data = await Villes.find();
-	console.log(data);
-	next();
+  const {
+    query: { letter }
+  } = req;
+
+  const data = await Villes.search(letter);
+  res.send(data);
+  next();
 }
 
 async function addVilles(req, res, next) {
-	console.log(req.body);
-
-	try {
-		await req.body.forEach(async element => {
-			await Villes.create(element);
-		});
-		res.send("added");
-		next();
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    await req.body.forEach(async element => {
+      await Villes.create(element);
+    });
+    res.send("added");
+    next();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function addVille(req, res, next) {
-	console.log(req.body);
-	console.log(req.params);
-	next();
+  console.log(req.body);
+  console.log(req.params);
+  next();
 }
 
 function testFn(req, res, next) {
-	res.send("gg get");
+  res.send("gg get");
 }
 
 function testPost(req, res, next) {
-	const { name } = req.body;
-	Villes.create({ name });
-	res.send("gg posted " + name);
+  const { name } = req.body;
+  Villes.create({ name });
+  res.send("gg posted " + name);
 
-	next();
+  next();
 }
 
 module.exports = addRoute;
